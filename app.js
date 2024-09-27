79e7553b0147ae1d781b081bcfd69a798712897e
muhammad-anas16 <anas.hanf38@gmail.com>
2024-09-27 13:15:32 -0700

    first commit

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-
-
 function per(num1){
-
-
-    return num1*100/500;
-
+    return num1 * 100 / 500;
 }
 
+var obtainedMarks = +prompt("Enter Obtained Marks out of 500"); // User input
+var percentage = per(obtainedMarks); // Calculate percentage
 
-
-
-var percentage = +prompt("Enter Obtained Mark")
-
-console.log(per(percentage))
-
-if(percentage < 85 && percentage > 100){
-    console.log("You Got A")
-}
-if(percentage < 70 && percentage > 85){
-    console.log("You Got B")
-}
-if(percentage < 60 && percentage > 70){
-    console.log("You Got C")
-}
-if(percentage < 55 && percentage > 70){
-    console.log("Pass")
+if (percentage <= 100 && percentage >= 85) {
+    console.log("You Got A");
+} else if (percentage < 85 && percentage >= 70) {
+    console.log("You Got B");
+} else if (percentage < 70 && percentage >= 60) {
+    console.log("You Got C");
+} else if (percentage < 60 && percentage >= 55) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
 }
